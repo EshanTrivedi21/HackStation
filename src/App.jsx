126821cf9476427b6376dfa11b0ec4dd08ba5952
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-28 00:10:54 +0530

    feat: mui init

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import Box from '@mui/material/Box';
 function App() {
   return (
     <>
+      <div className="w-screen h-screen flex justify-center items-center">
+      <Box
+      sx={{
+        p: 5,
+        bgcolor: '#1A1A1A',
+      }}
+      >
+        <h1 className="text-5xl text-[#CFCFCF]">HACKERSTELLAR</h1> 
+      </Box>
+      </div>
     </>
   )
 }
