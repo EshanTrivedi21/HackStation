53181ed1d00a1d24b1d207ffc1f0d1a63369fbb5
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-30 00:14:30 +0530

    feat: icon updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import Box from '@mui/material/Box';
-
 function App() {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
-        <Box
-          sx={{
-            p: 5,
-            bgcolor: "#fff",
-          }}
-        >
-          <h1 className="text-4xl text-[#101010]">HACKERSTELLAR</h1>
-        </Box>
+          <h1 className="text-4xl text-[#cfcfcf]">HACKERSTELLAR</h1>
       </div>
     </>
   );
