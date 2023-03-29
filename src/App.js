f8f78d336655e32c4502a4f75f94f5cee184b467
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-03-29 15:38:14 +0530

    feat: removed vite

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import Box from '@mui/material/Box';
+
 function App() {
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
-      <Box
-      sx={{
-        p: 5,
-        bgcolor: '#1A1A1A',
-      }}
-      >
-        <h1 className="text-5xl text-[#CFCFCF]">HACKERSTELLAR</h1> 
-      </Box>
+        <Box
+          sx={{
+            p: 5,
+            bgcolor: "#fff",
+          }}
+        >
+          <h1 className="text-4xl text-[#101010]">HACKERSTELLAR</h1>
+        </Box>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
