fd7a3d2104f247d28ceef66e02ae003e51607abd
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-04-06 14:00:38 +0530

    refactor: utils

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
--- src/utils/Theme.js
+++ src/utils/Theme.js
@@ -1,153 +1,153 @@
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
-  breakpoints: {
-    values: {
-      mobile: 0,
-      tablet: 640,
-      laptop: 1024,
-      desktop: 1200,
+    breakpoints: {
+        values: {
+            mobile: 0,
+            tablet: 640,
+            laptop: 1024,
+            desktop: 1200,
+        },
     },
-  },
-  palette: {
-    primary: {
-      main: "#101010",
-      contrastText: "#e8e8e8",
+    palette: {
+        primary: {
+            main: "#101010",
+            contrastText: "#e8e8e8",
+        },
+        secondary: {
+            main: "#F48037",
+            contrastText: "#101010",
+        },
     },
-    secondary: {
-      main: "#F48037",
-      contrastText: "#101010",
+    typography: {
+        login_title: {
+            fontSize: "1.8rem",
+            fontWeight: "600",
+            fontFamily: "Poppins, sans-serif",
+        },
+        login_subtitle: {
+            fontSize: "1.1rem",
+            fontWeight: "300",
+            fontFamily: "Poppins, sans-serif",
+            letterSpacing: "0.1rem",
+        },
+        modal_title: {
+            fontSize: "1.5rem",
+            fontWeight: "500",
+            fontFamily: "Poppins, sans-serif",
+            color: "#efefef",
+        },
+        modal_subtitle: {
+            fontSize: "1rem",
+            fontFamily: "Poppins, sans-serif",
+            color: "#cfcfcf",
+        },
+        greetings_title: {
+            fontSize: "1.2rem",
+            fontWeight: "500",
+            fontFamily: "Poppins, sans-serif",
+            color: "#efefef",
+            letterSpacing: "0.1rem",
+        },
+        greetings_subtitle: {
+            fontSize: "0.8rem",
+            fontWeight: "300",
+            fontFamily: "Poppins, sans-serif",
+            color: "#cfcfcf",
+        },
+        id_title: {
+            fontSize: "0.88rem",
+            fontWeight: "500",
+            fontFamily: "Poppins, sans-serif",
+            color: "#efefef",
+            letterSpacing: "0.1rem",
+        },
+        id_sub: {
+            fontSize: "0.65rem",
+            fontWeight: "300",
+            fontFamily: "Poppins, sans-serif",
+            color: "#cfcfcf",
+        },
+        timer: {
+            fontSize: "35px",
+            lineHeight: "1.4",
+            fontWeight: "500",
+            fontFamily: "Poppins, sans-serif",
+            color: "#d5d5d5",
+            letterSpacing: "0.1rem",
+        },
+        timer_title: {
+            fontSize: "15px",
+            fontWeight: "300",
+            fontFamily: "Poppins, sans-serif",
+            color: "#d5d5d5",
+        },
+        card_title: {
+            fontSize: "20px",
+            fontWeight: "500",
+            fontFamily: "Poppins, sans-serif",
+            color: "#efefef",
+        },
+        card_subtitle: {
+            fontSize: "11px",
+            fontWeight: "300",
+            fontFamily: "Poppins, sans-serif",
+            color: "#cfcfcf",
+        },
     },
-  },
-  typography: {
-    login_title: {
-      fontSize: "1.8rem",
-      fontWeight: "600",
-      fontFamily: "Poppins, sans-serif",
-    },
-    login_subtitle: {
-      fontSize: "1.1rem",
-      fontWeight: "300",
-      fontFamily: "Poppins, sans-serif",
-      letterSpacing: "0.1rem",
-    },
-    modal_title: {
-      fontSize: "1.5rem",
-      fontWeight: "500",
-      fontFamily: "Poppins, sans-serif",
-      color: "#efefef",
-    },
-    modal_subtitle: {
-      fontSize: "1rem",
-      fontFamily: "Poppins, sans-serif",
-      color: "#cfcfcf",
-    },
-    greetings_title: {
-      fontSize: "1.2rem",
-      fontWeight: "500",
-      fontFamily: "Poppins, sans-serif",
-      color: "#efefef",
-      letterSpacing: "0.1rem",
-    },
-    greetings_subtitle: {
-      fontSize: "0.8rem",
-      fontWeight: "300",
-      fontFamily: "Poppins, sans-serif",
-      color: "#cfcfcf",
-    },
-    id_title: {
-      fontSize: "0.88rem",
-      fontWeight: "500",
-      fontFamily: "Poppins, sans-serif",
-      color: "#efefef",
-      letterSpacing: "0.1rem",
-    },
-    id_sub: {
-      fontSize: "0.65rem",
-      fontWeight: "300",
-      fontFamily: "Poppins, sans-serif",
-      color: "#cfcfcf",
-    },
-    timer: {
-      fontSize: "35px",
-      lineHeight: "1.4",
-      fontWeight: "500",
-      fontFamily: "Poppins, sans-serif",
-      color: "#d5d5d5",
-      letterSpacing: "0.1rem",
-    },
-    timer_title: {
-      fontSize: "15px",
-      fontWeight: "300",
-      fontFamily: "Poppins, sans-serif",
-      color: "#d5d5d5",
-    },
-    card_title: {
-      fontSize: "20px",
-      fontWeight: "500",
-      fontFamily: "Poppins, sans-serif",
-      color: "#efefef",
-    },
-    card_subtitle: {
-      fontSize: "11px",
-      fontWeight: "300",
-      fontFamily: "Poppins, sans-serif",
-      color: "#cfcfcf",
-    },
-  },
 
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          height: "100%",
-          width: "100%",
-          borderRadius: "10px",
-          textTransform: "none",
-          letterSpacing: "0.1rem",
-          fontSize: "1.25rem",
-          fontFamily: "Poppins, sans-serif",
-          boxShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
-          padding: 0,
-          backgroundColor: "#151515 !important",
-          color: "#F48037",
-          "&:hover": {
-            backgroundColor: "#151515 !important",
-          },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "10px",
+                    textTransform: "none",
+                    letterSpacing: "0.1rem",
+                    fontSize: "1.25rem",
+                    fontFamily: "Poppins, sans-serif",
+                    boxShadow: "0px 5px 4px rgba(0, 0, 0, 0.36)",
+                    padding: 0,
+                    backgroundColor: "#151515 !important",
+                    color: "#F48037",
+                    "&:hover": {
+                        backgroundColor: "#151515 !important",
+                    },
+                },
+            },
         },
-      },
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          width: "75%",
-          "& .MuiInputBase-input": {
-            color: "#cfcfcf"
-          },
-          "& .MuiOutlinedInput-root": {
-            "& fieldset": {
-              borderRadius: "10px",
-              borderColor: "#505050",
+        MuiTextField: {
+            styleOverrides: {
+                root: {
+                    width: "75%",
+                    "& .MuiInputBase-input": {
+                        color: "#cfcfcf",
+                    },
+                    "& .MuiOutlinedInput-root": {
+                        "& fieldset": {
+                            borderRadius: "10px",
+                            borderColor: "#505050",
+                        },
+                        "&:hover fieldset": {
+                            borderColor: "#F48037",
+                        },
+                        "&.Mui-focused fieldset": {
+                            borderColor: "#F48037",
+                        },
+                    },
+                    "& .MuiFormLabel-root": {
+                        color: "#505050",
+                    },
+                    "& .MuiFormLabel-root.Mui-focused": {
+                        color: "#F48037",
+                    },
+                },
             },
-            "&:hover fieldset": {
-              borderColor: "#F48037",
-            },
-            "&.Mui-focused fieldset": {
-              borderColor: "#F48037",
-            },
-          },
-          "& .MuiFormLabel-root": {
-            color: "#505050",
-          },
-          "& .MuiFormLabel-root.Mui-focused": {
-            color: "#F48037",
-          },
         },
-      },
     },
-  },
 });
 
 export function Theme(props) {
-  return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
+    return <ThemeProvider theme={theme}>{props.children}</ThemeProvider>;
 }
