e1d5c5ea550b45d38b80ad5cd829472064d9c3d1
aryan <aaryan.p114@gmail.com>
2023-04-08 20:25:13 +0530

    feat: code cleanup

--- src/admin/Scan.jsx
+++ src/admin/Scan.jsx
@@ -6,40 +6,35 @@ import { Html5Qrcode } from "html5-qrcode";
 
 const Scan = () => {
     let [id, setId] = React.useState(null);
-    function handleScan(data) {
-        // if (data) alert(data);
-        // if (err)
-        // alert(err);
-    }
-    function handleError(err) {
-        alert(err);
-    }
-    const previewStyle = {
-        height: "300px",
-        width: "300px",
-        overflow: "hidden",
-    };
-    function onScanSuccess(decodedText, decodedResult) {
+    function onScanSuccess(decodedText) {
         setId(decodedText);
     }
 
     function onScanFailure(error) {
-        // handle scan failure, usually better to ignore and keep scanning.
-        // for example:
         console.warn(`Code scan error = ${error}`);
     }
     useEffect(() => {
-
-        const html5QrCode = new Html5Qrcode("reader", /* verbose= */ true);
-        const config = {  qrbox: { width: 200, height: 300 } };
-        html5QrCode.start({ facingMode: "environment" }, config, onScanSuccess, onScanFailure);
-
+        const html5QrCode = new Html5Qrcode("reader", true);
+        const config = { qrbox: { width: 200, height: 300 } };
+        html5QrCode.start(
+            { facingMode: "environment" },
+            config,
+            onScanSuccess,
+            onScanFailure
+        );
     }, []);
     return (
         <Container minHeight="auto" gap="4rem">
             <ScreenTitle title="Scan QR" />
             <FlexCol>
-                <div id="reader" style={previewStyle}></div>
+                <div
+                    id="reader"
+                    style={{
+                        height: "300px",
+                        width: "300px",
+                        overflow: "hidden",
+                    }}
+                ></div>
                 <p className="text-white">{id}</p>
             </FlexCol>
             <FlexCol>
