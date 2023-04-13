4ddd88072eeb44d3e5898e3d05230b48e6dbb336
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-04-14 01:37:31 +0530

    feat: pref form added

--- src/screens/PS.jsx
+++ src/screens/PS.jsx
@@ -5,7 +5,14 @@ import { Typography } from "@mui/material";
 import { AdminControlContext } from "../contexts/adminControlContext";
 
 const PS = () => {
-    let stateAC = useContext(AdminControlContext)
+    let stateAC = useContext(AdminControlContext);
+    const handleClick = () => {
+        if (stateAC.adminData["ps-form"]) {
+            window.open("https://forms.gle/M2ugv3eJhfc7haNN9", "_blank");
+        } else {
+            return;
+        }
+    };
     return (
         <>
             <Container className="!justify-start" gap="3rem" overflow="hidden">
@@ -14,15 +21,18 @@ const PS = () => {
                     <Typography variant="card_title" className="!ml-10">
                         Form
                     </Typography>
-                    {stateAC.adminData && <Card
-                        width="30"
-                        height="30"
-                        title="Preference Form"
-                        subtitle="Fill the form to get your preferences"
-                        icon="/icons/psform.svg"
-                        button={true}
-                        disabled={!stateAC.adminData["ps-form"]}
-                    />}
+                    {stateAC.adminData && (
+                        <Card
+                            width="30"
+                            height="30"
+                            title="Preference Form"
+                            subtitle="Fill the form to get your preferences"
+                            icon="/icons/psform.svg"
+                            button={true}
+                            onClick={handleClick}
+                            disabled={!stateAC.adminData["ps-form"]}
+                        />
+                    )}
                 </FlexCol>
                 <FlexCol className="gap-5 !items-start">
                     <Typography variant="card_title" className="!ml-10">
