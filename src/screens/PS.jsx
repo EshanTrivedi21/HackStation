f0e5487c04ca46e92e8062887904529851a3d0c8
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-04-14 04:11:02 +0530

    feat: github form

--- src/screens/PS.jsx
+++ src/screens/PS.jsx
@@ -1,39 +1,25 @@
 import React, { useContext } from "react";
 import { Container, ScreenTitle, Card, FlexCol } from "../utils/Utilities";
 import data from "../data/ps.json";
+import forms from "../data/forms.json";
 import { Typography } from "@mui/material";
 import { AdminControlContext } from "../contexts/adminControlContext";
 
 const PS = () => {
     let stateAC = useContext(AdminControlContext);
-    const handleClick = () => {
-        if (stateAC.adminData["ps-form"]) {
-            window.open("https://forms.gle/M2ugv3eJhfc7haNN9", "_blank");
+
+    const handleClick = (link, check) => {
+        if (stateAC.adminData[check]) {
+            window.open(link, "_blank");
         } else {
             return;
         }
     };
+
     return (
         <>
             <Container className="!justify-start" gap="3rem" overflow="hidden">
-                <ScreenTitle title="Problems" />
-                <FlexCol className="gap-5 !items-start">
-                    <Typography variant="card_title" className="!ml-10">
-                        Form
-                    </Typography>
-                    {stateAC.adminData && (
-                        <Card
-                            width="30"
-                            height="30"
-                            title="Preference Form"
-                            subtitle="Fill the form to get your preferences"
-                            icon="/icons/psform.svg"
-                            button={true}
-                            onClick={handleClick}
-                            disabled={!stateAC.adminData["ps-form"]}
-                        />
-                    )}
-                </FlexCol>
+                <ScreenTitle title="PS" />
                 <FlexCol className="gap-5 !items-start">
                     <Typography variant="card_title" className="!ml-10">
                         Domains
@@ -49,6 +35,27 @@ const PS = () => {
                             button={true}
                         />
                     ))}
+                </FlexCol>
+                <FlexCol className="gap-5 !items-start">
+                    <Typography variant="card_title" className="!ml-10">
+                        Forms 
+                    </Typography>
+                    {stateAC.adminData &&
+                        forms.map((item, index) => (
+                            <Card
+                                key={index}
+                                width="30"
+                                height="30"
+                                title={item.title}
+                                subtitle={item.subtitle}
+                                icon={item.icon}
+                                button={true}
+                                onClick={() => {
+                                    handleClick(item.link, item.query);
+                                }}
+                                disabled={!stateAC.adminData[item.query]}
+                            />
+                        ))}
                 </FlexCol>
             </Container>
         </>
